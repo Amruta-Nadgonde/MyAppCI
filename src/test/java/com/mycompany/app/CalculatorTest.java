c5483ea6c7b7a0416cc1746e6ecffd976745b496
Amruta-Nadgonde <amruta.nadgonde@gmail.com>
2023-08-04 17:02:13 +0530

    First Commit

---
+++ src/test/java/com/mycompany/app/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.mycompany.app;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.mycompany.app.Calculator;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		int exp = 30;
+		Calculator cal = new Calculator();
+		int act = cal.add(10, 20);
+		assertEquals(exp, act);
+	}
+	
+	@Test
+	public void testmultiply() {
+		int exp = 300;
+		Calculator cal = new Calculator();
+		int act = cal.multiply(10, 30);
+		assertEquals(exp, act);
+	}
+	
+	@Test
+	public void testDivide() {
+		int exp = 3;
+		Calculator cal = new Calculator();
+		int act = cal.divide(30,10);
+		assertEquals(exp, act);
+	}
+	
+	
+	
+
+}
